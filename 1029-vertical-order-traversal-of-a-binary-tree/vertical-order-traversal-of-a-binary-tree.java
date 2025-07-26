48e5a792ac2f2cf841e7edec4712b9aee1c27641
Lavlesh Pandey <116619355+lavlesh349@users.noreply.github.com>
2025-07-26 22:58:20 +0530

    Time: 5 ms (8.86%) | Memory: 42.7 MB (34.81%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -17,49 +17,45 @@ class Solution {
     class pair{
         TreeNode node;
         int idx;
-
-        pair(TreeNode node,int idx){
+        int level;
+        pair(TreeNode node,int idx,int level){
             this.node=node;
             this.idx=idx;
+            this.level=level;
         }
         pair(){}
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer,List<Integer>> map=new TreeMap<>();
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap<>();
         Queue<pair> q=new LinkedList<>();
         if(root!=null){
-            q.add(new pair(root,0));
+            q.add(new pair(root,0,0));
         }
         while(!q.isEmpty()){
-            int size=q.size();
-            HashMap<Integer,List<Integer>> temp=new HashMap<>();
-            while(size-->0){
-                pair rv=q.poll();
-                if(!temp.containsKey(rv.idx)){
-                    temp.put(rv.idx,new ArrayList<>());
-                }
-                temp.get(rv.idx).add(rv.node.val);
-                if(rv.node.left!=null){
-                    q.add(new pair(rv.node.left,rv.idx-1));
-                }
-                if(rv.node.right!=null){
-                    q.add(new pair(rv.node.right,rv.idx+1));
-                }
+            pair rv=q.poll();
+            if(!map.containsKey(rv.idx)){
+                map.put(rv.idx,new TreeMap<>());
             }
-            for(int key:temp.keySet()){
-                List<Integer> temp2=temp.get(key);
-                Collections.sort(temp2);
-                if(!map.containsKey(key)){
-                    map.put(key,new ArrayList<>());
-                }
-                for(int i:temp2){
-                    map.get(key).add(i);
-                }
+            if(!map.get(rv.idx).containsKey(rv.level)){
+                map.get(rv.idx).put(rv.level,new PriorityQueue<>());
+            }
+            map.get(rv.idx).get(rv.level).add(rv.node.val);
+            if(rv.node.left!=null){
+                q.add(new pair(rv.node.left,rv.idx-1,rv.level+1));
+            }
+            if(rv.node.right!=null){
+                q.add(new pair(rv.node.right,rv.idx+1,rv.level+1));
             }
         }
         List<List<Integer>> ans=new ArrayList<>();
         for(int key:map.keySet()){
-            ans.add(new ArrayList(map.get(key)));
+            List<Integer> ll=new ArrayList<>();
+            for(int key2:map.get(key).keySet()){
+                while(!map.get(key).get(key2).isEmpty()){
+                    ll.add(map.get(key).get(key2).poll());
+                }
+            }
+            ans.add(new ArrayList<>(ll));
         }
         return ans;
     }
