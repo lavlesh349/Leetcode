8b0677bda7159c845e11995ab32ba9c1d855bf64
Lavlesh Pandey <116619355+lavlesh349@users.noreply.github.com>
2025-07-31 17:11:59 +0530

    Time: 15 ms (50.17%) | Memory: 46.5 MB (21.28%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -11,47 +11,53 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        return convert(root);
+        if(root==null)return "";
+        StringBuilder str=new StringBuilder();
+        Queue<TreeNode> q=new LinkedList<>();
+        str.append(String.valueOf(root.val));
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode rv=q.poll();
+            if(rv.left!=null){
+                str.append(","+String.valueOf(rv.left.val));
+                q.add(rv.left);
+            }
+            else{
+                str.append(",*");
+            }
+            if(rv.right!=null){
+                str.append(","+String.valueOf(rv.right.val));
+                q.add(rv.right);
+            }
+            else{
+                str.append(",*");
+            }
+        }
+        return str.toString();
     }
 
-    public String convert(TreeNode root){
-        if(root==null){
-            return "";
-        }
-        StringBuilder ll=new StringBuilder();
-        ll.append(String.valueOf(root.val));
-        if(root.left==null){
-            ll.append(" False");
-        }
-        else{
-            ll.append(" True "+convert(root.left));
-        }
-        if(root.right==null){
-            ll.append(" False");
-        }
-        else{
-            ll.append(" True "+convert(root.right));
-        }
-        return ll.toString();
-    }
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         if(data.length()==0){
             return null;
         }
-        String[] arr=data.split(" ");
-        // System.out.println(Arrays.toString(arr));
-        return construct(arr,new int[]{0});
-    }
-
-    public TreeNode construct(String[] arr,int[] idx){
-        if(idx[0]>=arr.length)return null;
-        TreeNode root=new TreeNode(Integer.parseInt(arr[idx[0]++]));
-        if(arr[idx[0]++].equals("True")){
-            root.left=construct(arr,idx);
-        }
-        if(arr[idx[0]++].equals("True")){
-            root.right=construct(arr,idx);
+        String[] arr=data.split(",");
+        TreeNode root=new TreeNode(Integer.parseInt(arr[0]));
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int idx=1;
+        while(!q.isEmpty()){
+            TreeNode rv=q.poll();
+            if(!arr[idx].equals("*")){
+                rv.left=new TreeNode(Integer.parseInt(arr[idx]));
+                q.add(rv.left);
+            }
+            idx++;
+            if(!arr[idx].equals("*")){
+                rv.right=new TreeNode(Integer.parseInt(arr[idx]));
+                q.add(rv.right);
+            }
+            idx++;
         }
         return root;
     }
